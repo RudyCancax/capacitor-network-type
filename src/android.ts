c4b248b6aec3516f887b0c1b7fc2e859cac7537b
rudycancax <44583578+RudyCancax@users.noreply.github.com>
2024-08-27 12:14:19 -0600

    Fix: Modifications for types

--- src/android.ts
+++ src/android.ts
@@ -1,16 +1,31 @@
-import { Plugins } from '@capacitor/core';
+import { WebPlugin } from '@capacitor/core';
 import type { NetworkTypePlugin } from './definitions';
 
-const { NetworkType } = Plugins;
-
-export class NetworkTypeAndroid implements NetworkTypePlugin {
+export class NetworkTypeAndroid extends WebPlugin implements NetworkTypePlugin {
   async getNetworkType(): Promise<{ networkType: string }> {
-    try {
-      const { networkType } = await NetworkType.getNetworkType();
-      return { networkType };
-    } catch (error) {
-      console.error('Failed to get network type:', error);
-      return { networkType: 'Unknown' };
-    }
+    console.log('Running on the web, returning default value');
+    return { networkType: 'Unknown' };
+  }
+
+  async echo(options: { value: string }): Promise<{ value: string }> {
+    console.log('ECHO on Android', options);
+    return options;
   }
 }
+
+// import { Plugins } from '@capacitor/core';
+// import type { NetworkTypePlugin } from './definitions';
+
+// const { NetworkType } = Plugins;
+
+// export class NetworkTypeAndroid implements NetworkTypePlugin {
+//   async getNetworkType(): Promise<{ networkType: string }> {
+//     try {
+//       const { networkType } = await NetworkType.getNetworkType();
+//       return { networkType };
+//     } catch (error) {
+//       console.error('Failed to get network type:', error);
+//       return { networkType: 'Unknown' };
+//     }
+//   }
+// }
--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,22 @@
 import { WebPlugin } from '@capacitor/core';
 import type { NetworkTypePlugin } from './definitions';
 
+// export class NetworkTypeWeb extends WebPlugin implements NetworkTypePlugin {
+//   async getNetworkType(): Promise<{ networkType: string }> {
+//     console.log('Running on the web, returning default value');
+//     return { networkType: 'Unknown' };
+//   }
+// }
+
 export class NetworkTypeWeb extends WebPlugin implements NetworkTypePlugin {
   async getNetworkType(): Promise<{ networkType: string }> {
     console.log('Running on the web, returning default value');
     return { networkType: 'Unknown' };
+  }
+
+  async echo(options: { value: string }): Promise<{ value: string }> {
+    console.log('ECHO on Web', options);
+    return options;
   }
 }
 
